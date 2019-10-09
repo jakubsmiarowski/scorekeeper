af5d193cf1bceecaec4a6b207f0ef21946af16fc
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-10-09 18:09:35 +0200

    add remove player

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 it('renders without crashing', () => {
     shallow( <App /> );
@@ -24,4 +25,17 @@ it('should update player score', () => {
     const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
     onScoreUpdate(0, 5);
     const playersAfterUpdate = appComponent.state('players');
+
+    expect(playersAfterUpdate).toEqual()
 });
+
+it('should add player to state', () => {
+    const appComponent = shallow(<App />);
+    const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
+    onPlayerAdd('Ania');
+    const players = appComponent.state('players');
+
+    expect(players.length).toEqual(1);
+    expect(players[0].name).toEqual('Ania');
+    expect(players[0].score).toEqual(0);
+})
--- src/components/PlayersList/PlayersList.js
+++ src/components/PlayersList/PlayersList.js
@@ -10,6 +10,7 @@ const PlayersList = (props) => (
                 name={player.name}
                 score={player.score}
                 onPlayerScoreChange={(points) => props.onScoreUpdate(i, points)}
+                onPlayerRemove={(player) => props.onPlayerUpdate(i, player)}
                 />)
             )}
     </ul>
